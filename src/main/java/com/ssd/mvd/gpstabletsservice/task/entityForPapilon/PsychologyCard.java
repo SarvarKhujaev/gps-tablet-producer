8dca4f9f00d6b41bc7f61a5df4a4a41bb75acfb1
Sarvar Khujaev <sarvar.khujaev@ssd.uz>
2022-08-08 17:06:31 +0500

    CarEvents and FaceEvents were updated

--- src/main/java/com/ssd/mvd/gpstabletsservice/task/entityForPapilon/PsychologyCard.java
+++ src/main/java/com/ssd/mvd/gpstabletsservice/task/entityForPapilon/PsychologyCard.java
@@ -1,6 +1,7 @@
 package com.ssd.mvd.gpstabletsservice.task.entityForPapilon;
 
 import com.ssd.mvd.gpstabletsservice.task.entityForPapilon.modelForAddress.ModelForAddress;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import java.util.List;
@@ -10,15 +11,22 @@ import lombok.Data;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PsychologyCard {
+    @JsonDeserialize
     private Pinpp pinpp;
     private String personImage; // the image of the person
 
+    @JsonDeserialize
     private List< PapilonData > papilonData;
+    @JsonDeserialize
     private List< Violation > violationList;
 
+    @JsonDeserialize
     private ModelForCarList modelForCarList; // the list of all cars which belongs to this person
+    @JsonDeserialize
     private ModelForAddress modelForAddress;
 
+    @JsonDeserialize
     private com.ssd.mvd.gpstabletsservice.task.entityForPapilon.modelForCadastr.Data modelForCadastr;
+    @JsonDeserialize
     private com.ssd.mvd.gpstabletsservice.task.entityForPapilon.modelForPassport.Data modelForPassport;
 }
